8310508e1be1722d324652f9c06da8302f18057b
Yaryna Laiter <91200119+yaryna-laiter@users.noreply.github.com>
2026-02-14 20:55:08 +0100

    feat(login): create login page with form, validation, JWT storage, and redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/LoginPage';
+import DecksPage from './pages/DeckPage';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/decks">Decks</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/decks" element={<DecksPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
